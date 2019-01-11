fbbaab26ac38d72c9d310101156961e1d79ce2ba
Márcio <marcio.quimbundo@ucall.co.ao>
2019-01-11 16:22:50 +0100

    song title, artist name, and controls added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:music_player/theme.dart';
 
 void main() =>
     runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Home()));
@@ -34,7 +35,9 @@ class _HomeState extends State<Home> {
       body: Column(
         children: <Widget>[
           //seek bar
-
+          Expanded(
+            child: Container(),
+          ),
           // visualize
           Container(
             width: double.infinity,
@@ -42,39 +45,79 @@ class _HomeState extends State<Home> {
           ),
           // song title, artist name, and controls
           Container(
-            color: Colors.black,
-            child: Column(
-              children: <Widget>[
-                RichText(
-                  text: TextSpan(
-                    text: '',
-                    children: [
-                      TextSpan(
-                        text: 'Song Title\n',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 14.0,
-                          fontWeight: FontWeight.bold,
-                          letterSpacing: 4.0,
-                          height: 1.5,
+            color: accentColor,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 40.0, bottom: 50.0),
+              child: Column(
+                children: <Widget>[
+                  RichText(
+                    text: TextSpan(
+                      text: '',
+                      children: [
+                        TextSpan(
+                          text: 'Song Title\n',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: 4.0,
+                            height: 1.5,
+                          ),
                         ),
-                      ),
-                      TextSpan(
-                        text: 'Artist Name',
-                        style: TextStyle(
-                          color: Colors.white.withOpacity(0.75),
-                          fontSize: 12.0,
-                          letterSpacing: 3.0,
-                          height: 1.5,
+                        TextSpan(
+                          text: 'Artist Name',
+                          style: TextStyle(
+                            color: Colors.white.withOpacity(0.75),
+                            fontSize: 12.0,
+                            letterSpacing: 3.0,
+                            height: 1.5,
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-                Row(
-                  children: <Widget>[],
-                )
-              ],
+                  Padding(
+                    padding: const EdgeInsets.only(top: 40.0),
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(child: Container()),
+                        IconButton(
+                          icon: Icon(Icons.skip_previous),
+                          color: Colors.white,
+                          iconSize: 36.5,
+                          onPressed: () {},
+                        ),
+                        Expanded(child: Container()),
+                        RawMaterialButton(
+                          shape: CircleBorder(),
+                          fillColor: Colors.white,
+                          splashColor: lightAccentColor,
+                          highlightColor: lightAccentColor.withOpacity(0.5),
+                          elevation: 10.0,
+                          highlightElevation: 5.0,
+                          onPressed: () {},
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Icon(
+                              Icons.play_arrow,
+                              color: darkAccentColor,
+                              size: 35.0,
+                            ),
+                          ),
+                        ),
+                        Expanded(child: Container()),
+                        IconButton(
+                          icon: Icon(Icons.skip_next),
+                          color: Colors.white,
+                          iconSize: 36.5,
+                          onPressed: () {},
+                        ),
+                        Expanded(child: Container()),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
