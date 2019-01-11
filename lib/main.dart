928e6d4ccad8a72abf4db45651948ce9b0da56dc
Márcio <marcio.quimbundo@ucall.co.ao>
2019-01-11 15:05:51 +0100

    arrow_back_ios icon and menu at the top, cleaning the appBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(Home());
+void main() =>
+    runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Home()));
 
 class Home extends StatefulWidget {
   @override
-    State<StatefulWidget> createState() {
-      return _HomeState();
-    }
+  State<StatefulWidget> createState() {
+    return _HomeState();
+  }
 }
 
 class _HomeState extends State<Home> {
@@ -14,7 +15,21 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Music Player'),
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios),
+          color: const Color(0xFFDDDDDD),
+          onPressed: () {},
+        ),
+        title: Text(''),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.menu),
+            color: const Color(0xFFDDDDDD),
+            onPressed: () {},
+          ),
+        ],
       ),
       body: Center(),
     );
