a9dbeeccbd04d2a1dc59f808b981a9b8604ad88a
Márcio <marcio.quimbundo@ucall.co.ao>
2019-01-29 09:23:53 +0100

    _renderHistogram finished

--- lib/main.dart
+++ lib/main.dart
@@ -102,10 +102,49 @@ class VisualizerPainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
-    canvas.drawRect(
-      Rect.fromLTWH(0.0, 0.0, size.width, size.height),
-      wavePaint
-    );
+    _renderWaves(canvas, size);
+  }
+
+  void _renderWaves(Canvas canvas, Size size) {
+    final histogramLow = _createHistogram(fft, 15, 2, ((fft.length) / 4).floor());
+    final histogramHigh = _createHistogram(fft, 15, (fft.length / 4).ceil(), ((fft.length) / 2).floor());
+
+    _renderHistogram(canvas, size, histogramLow);
+    _renderHistogram(canvas, size, histogramHigh);
+  }
+  void _renderHistogram(Canvas canvas, Size size, List<int> histogram) {
+    if (histogram.length == 0) {
+      return;
+    }
+    
+    final pointsToGraph = histogram.length;
+    final widthPerSample = (size.width / (pointsToGraph - 2)).floor();
+
+    final points = new List<double>.filled(pointsToGraph * 4, 0.0);
+
+    for (int i = 0; i < histogram.length - 1; ++i) {
+      points[i * 4] = (i * widthPerSample).toDouble();
+      points[i * 4 + 1] = size.height - histogram[i].toDouble();
+
+      points[i * 4 + 2] = ((i + 1) * widthPerSample).toDouble();
+      points[i * 4 + 3] = size.height - (histogram[i + 1].toDouble());
+    }
+
+    Path path = new Path();
+    path.moveTo(0.0, size.height);
+    path.lineTo(points[0], points[1]);
+
+    for (int i = 2; i < points.length - 4; i += 2) {
+      path.cubicTo(
+        points[i - 2] + 10.0, points[i - 1],
+        points[i] - 10.0, points [i + 1],
+        points[i], points[i + 1],
+      );
+    }
+    path.lineTo(size.width, size.height);
+    path.close(); 
+
+    canvas.drawPath(path, wavePaint);
   }
   List<int> _createHistogram(List<int> samples, int bucketCount, [int start, int end]) {
     if(start == end) {
